779e24e2c27daacafcd6afce7c82968f7fd969e5
jyh071116 <jyh071116@gmail.com>
2023-08-02 23:20:37 +0900

    fix : 세팅버그수정

--- App.jsx
+++ App.jsx
@@ -3,23 +3,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Quiz from "./pages/Quiz";
 import Dictionary from "./pages/Dictionary";
-import Camera from "pages/Camera";
-import Main from "./pages/main";
+import Camera from "./pages/Camera";
+import Main from "./pages/Main";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName="Main">
-          <Tab.Screen name="Main" component={Main} />
-          <Tab.Screen name="Quiz" component={Quiz} />
-          <Tab.Screen name="Camera" component={Camera} />
-          <Tab.Screen name="Dictionary" component={Dictionary} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Main" component={Main} />
+        <Tab.Screen name="Quiz" component={Quiz} />
+        <Tab.Screen name="Camera" component={Camera} />
+        <Tab.Screen name="Dictionary" component={Dictionary} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
--- pages/dictionary.jsx
+++ pages/dictionary.jsx
@@ -1,3 +1,5 @@
+import { Text, View } from "react-native";
+
 export default function Dictionary() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
